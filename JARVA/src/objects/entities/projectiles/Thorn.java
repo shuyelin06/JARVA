e1d1bbe0a0a66f5381a603f07f003ed85add78ba
SvenJing <stevenying2004@gmail.com>
2022-05-03 18:16:47 -0400

    Unmessed up thorn

--- JARVA/src/objects/entities/projectiles/Thorn.java
+++ JARVA/src/objects/entities/projectiles/Thorn.java
@@ -15,7 +15,6 @@ import objects.geometry.Polygon;
 import ui.display.images.ImageManager;
 
 public class Thorn extends Projectile {
-	
 	private float theta;
 	
 	private GameObject origin;
@@ -32,7 +31,7 @@ public class Thorn extends Projectile {
 		this.setTeam(origin.getTeam());
 		
 		this.sprite = ImageManager.getImageCopy("thorn", 2, 2);
-		
+
 		this.pierce = 1;
 		this.knockback = 0;
 		this.damageMultiplier = 1;
@@ -47,55 +46,6 @@ public class Thorn extends Projectile {
 	public void projectileUpdate() {
 		this.addXVelocity((float) Math.cos(theta) * 2.5f);
 		this.addYVelocity((float) Math.sin(theta) * 2.5f);
-		this.knockback = 60f;
-		timer++;
-		
-		// if this is a primary source thorn, it will home for a bit and then split in 3
-		if (origin instanceof Thorn) {
-			if (this.getXVelocity() == 0) {
-				this.setXVelocity((float) Math.cos(theta) * baseSpeed);
-				this.setYVelocity((float) Math.sin(theta) * baseSpeed);
-			}
-		} else {
-			if (timer < maxTimer) {
-				theta = Math.atan2(target.getY() - origin.getY(), target.getX() - origin.getX());
-				this.setXVelocity((float) Math.cos(theta) * baseSpeed);
-				this.setYVelocity((float) Math.sin(theta) * baseSpeed);
-			} else if (timer == maxTimer) {
-				new Thorn(this, null)
-					.setMaxTimer(500)
-					.setBaseSpeed(this.baseSpeed * 3)
-					.setAngle((float) (theta + (Math.PI / 6)))
-					.setPierce(1)
-					.setKnockback(0)
-					.setDamageMultiplier(1)
-					.build();
-				
-				new Thorn(this, null)
-					.setMaxTimer(500)
-					.setBaseSpeed(this.baseSpeed * 3)
-					.setAngle((float) (theta))
-					.setPierce(1)
-					.setKnockback(0)
-					.setDamageMultiplier(1)
-					.build();
-				
-				new Thorn(this, null)
-					.setMaxTimer(500)
-					.setBaseSpeed(this.baseSpeed * 3)
-					.setAngle((float) (theta - (Math.PI / 6)))
-					.setPierce(1)
-					.setKnockback(0)
-					.setDamageMultiplier(1)
-					.build();
-				
-				//remove primary projectile
-				this.remove();
-			}
-		}
-		
-		sprite.setCenterOfRotation(sprite.getWidth() * 0.5f, sprite.getHeight() * 0.5f);
-		sprite.rotate((float) Math.toDegrees(theta));
 	}
 
 	@Override
